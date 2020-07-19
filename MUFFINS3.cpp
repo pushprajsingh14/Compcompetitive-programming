f71e760518f5450ae466386b8f8ae56ae139780b
Pushpraj Singh <60061416+pushprajsingh14@users.noreply.github.com>
2020-07-20 01:01:39 +0530

    Create MUFFINS3.cpp

---
+++ MUFFINS3.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/MUFFINS3
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<(n/2)+1<<endl;
+    }
+    return 0;
+}
