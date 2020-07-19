81a33ff47f3a5cdbd5b177ec12583fa8b349ca36
Pushpraj Singh <60061416+pushprajsingh14@users.noreply.github.com>
2020-07-20 00:53:38 +0530

    Create START01.cpp

---
+++ START01.cpp
@@ -0,0 +1,12 @@
+question: https://www.codechef.com/problems/START01
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	cout<<n;
+	return 0;
+}
