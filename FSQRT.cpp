2eb0a50de84ee582b56786ac9388e76d8a2a71bb
Pushpraj Singh <60061416+pushprajsingh14@users.noreply.github.com>
2020-07-20 01:02:14 +0530

    Create FSQRT.cpp

---
+++ FSQRT.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/FSQRT
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a;
+	    cin>>a;
+	    cout<<int(sqrt(a))<<endl;
+	}
+	return 0;
+}
