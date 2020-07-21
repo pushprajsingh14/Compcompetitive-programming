d768b9d9638c0c4a2fad9aceff5918ff756c6c30
Pushpraj Singh <60061416+pushprajsingh14@users.noreply.github.com>
2020-07-21 15:34:41 +0530

    Create FLOW008.cpp

---
+++ FLOW008.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/FLOW008
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n<10)
+	    cout<<"What an obedient servant you are!"<<endl;
+	    else
+	    cout<<"-1"<<endl;
+	}
+	return 0;
+}
