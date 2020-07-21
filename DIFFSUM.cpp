e4746af1addffb328c0dc43dc587bf9a607a35d6
Pushpraj Singh <60061416+pushprajsingh14@users.noreply.github.com>
2020-07-21 15:21:38 +0530

    Create DIFFSUM.cpp

---
+++ DIFFSUM.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/DIFFSUM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a>b)
+	cout<<a-b;
+	else
+	cout<<a+b;
+	
+	return 0;
+}
